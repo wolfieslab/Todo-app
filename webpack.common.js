6ea8a15be0af6acf240b279b7e6389e0e43dbc3a
wolfieslab <214845866+wolfieslab@users.noreply.github.com>
2026-03-13 19:28:07 +0530

    build: add webpack common configuration

---
+++ webpack.common.js
@@ -0,0 +1,23 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+export default {
+    entry: {
+        app: './src/index.js',
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path: path.resolve(import.meta.dirname, 'dist'),
+        clean: true,
+    },
+};

60ff6dc20b979324052432ef9ec19c1e401c6916
wolfieslab <214845866+wolfieslab@users.noreply.github.com>
2026-03-14 14:21:23 +0530

    feat: initialize default project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import "./styles.css";
+import createTodo from "../modules/todo";
+import createProject from "../modules/project";
 
-console.log("this is index.js");
+const homeProject = createProject("Home");
+const todo1 = createTodo("Buy Groceries", "vegetables, essential food items", "2026-03-15", "High");
+const todo2 = createTodo("Complete odin project", "todo app project", "2026-03-15", "High");
+homeProject.addTodo(todo1.getData());
+homeProject.addTodo(todo2.getData());
+
+console.log(homeProject.name, homeProject.getTodos());

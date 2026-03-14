262bcdbd4a41b18dd98791cb8667a56f1dd1a932
wolfieslab <214845866+wolfieslab@users.noreply.github.com>
2026-03-14 12:40:44 +0530

    feat: implement project module to manage todo

---
+++ modules/project.js
@@ -0,0 +1,9 @@
+export default function createProject(name) {
+    let todos = [];
+
+    const addTodo = (todo) => todos.push(todo);
+    const removeTodo = (index) => todos.splice(index, 1);
+    const getTodos = () => { return todos };
+
+    return { name, getTodos, addTodo, removeTodo };
+}

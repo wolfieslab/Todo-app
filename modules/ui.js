333476d6f4fa43bc8beb326638f221d7fca66147
wolfieslab <214845866+wolfieslab@users.noreply.github.com>
2026-03-16 20:25:43 +0530

    feat: implement custom checkbox for todos

--- modules/ui.js
+++ modules/ui.js
@@ -5,6 +5,30 @@ export function renderProjects() {
     const sidebar = document.getElementById("projects");
     sidebar.innerHTML = "";
 
+    const logo = document.createElement("div");
+    logo.classList.add("logo");
+    logo.textContent = "Todo";
+
+    const addTaskBtn = document.createElement("button")
+    addTaskBtn.classList.add("add-task-btn");
+    addTaskBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus-circle</title><path d="M17,13H13V17H11V13H7V11H11V7H13V11H17M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>
+        <span>Add Task</span>`;
+
+    const heading = document.createElement("div");
+    heading.classList.add("heading")
+
+    const headingText = document.createElement("div");
+    headingText.classList.add("heading-text");
+    headingText.textContent = "My Projects";
+
+    const addProjectBtn = document.createElement("button");
+    addProjectBtn.classList.add("add-project-btn");
+    addProjectBtn.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>plus</title><path d="M19,13H13V19H11V13H5V11H11V5H13V11H19V13Z" /></svg>`;
+    heading.append(headingText, addProjectBtn);
+
+    sidebar.append(logo, addTaskBtn, heading);
+
     getProjects().forEach((project, index) => {
         const btn = document.createElement("button");
         btn.textContent = project.name;
@@ -12,12 +36,6 @@ export function renderProjects() {
 
         sidebar.appendChild(btn);
     });
-
-    const addProjectBtn = document.createElement("button");
-    addProjectBtn.textContent = "+ Add Project";
-    addProjectBtn.classList.add("add-project-btn");
-
-    sidebar.appendChild(addProjectBtn);
 }
 
 export function renderTodos(project) {
@@ -25,21 +43,45 @@ export function renderTodos(project) {
     list.innerHTML = "";
 
     const heading = document.createElement("h2");
-    heading.classList.add("heading");
+    heading.classList.add("title");
     heading.textContent = project.name;
 
     list.appendChild(heading);
 
-    project.getTodos().forEach((todo, index) => {
-        const item = document.createElement("div");
-        item.classList.add("todo");
-
-        item.innerHTML =
-        `
-            <h3>${todo.getData().title}</h3>
-            <p>${todo.getData().description}</p>
-            <p><span>${todo.getData().dueDate}</span></p>
-        `;
+    project.getTodos().forEach(todo => {
+        const item = createTodoElement(todo);
         list.appendChild(item);
     });
 }
+
+function createTodoElement(todo) {
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+
+    const todoCheckbox = document.createElement("label");
+    todoCheckbox.classList.add("todo-checkbox");
+
+    todoCheckbox.innerHTML = `
+            <input type="checkbox">
+            <span class="checkmark">
+                <svg class="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>check</title><path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" /></svg>
+            </span>
+        `;
+
+    const todoDetails = document.createElement("div");
+    todoDetails.classList.add("todo-details");
+
+    todoDetails.innerHTML = `
+        <h4>${todo.getData().title}</h4>
+        <p>${todo.getData().description}</p>
+        <p>
+            <span>${todo.getData().dueDate}</span>
+            <span>Priority: ${todo.getData().priority}</span>
+        </p>
+        <button class="delete-todo-btn">Delete</button>
+    `;
+
+    todoDiv.append(todoCheckbox, todoDetails);
+    return todoDiv;
+}
+

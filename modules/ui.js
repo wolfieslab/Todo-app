ccf206528d5a0d9b7d799377214f9bb8e2abac57
wolfieslab <214845866+wolfieslab@users.noreply.github.com>
2026-03-17 15:47:51 +0530

    feat: implement add project functionalities

--- modules/ui.js
+++ modules/ui.js
@@ -1,4 +1,5 @@
-import { getProjects } from "./appController";
+import { createProjects, getProjects } from "./appController";
+import project from "./project"
 import todo from "./todo";
 
 export function renderProjects() {
@@ -30,11 +31,26 @@ export function renderProjects() {
     sidebar.append(logo, addTaskBtn, heading);
 
     getProjects().forEach((project, index) => {
-        const btn = document.createElement("button");
-        btn.textContent = project.name;
-        btn.dataset.index = index;
+        const div = document.createElement("div");
+        const span = document.createElement("span");
+        span.textContent = project.name;
 
-        sidebar.appendChild(btn);
+        const deleteBtn = document.createElement("button");
+        deleteBtn.classList.add("delete-project-btn")
+        
+        div.dataset.index = index;
+        div.append(span, deleteBtn);
+
+        sidebar.appendChild(div);
+    });
+
+
+    addProjectBtn.addEventListener("click",() => {
+        openProjectModal(createProjects);
+    });
+
+    addTaskBtn.addEventListener("click", () => {
+        openTaskModal(project.addTodo);
     });
 }
 
@@ -83,5 +99,97 @@ function createTodoElement(todo) {
 
     todoDiv.append(todoCheckbox, todoDetails);
     return todoDiv;
+}
+
+function createModal({ title, fields, onSubmit }) {
+    const dialog = document.createElement("dialog");
+    dialog.classList.add("modal");
+
+    const form = document.createElement("form");
+    form.method = "dialog";
+
+    const heading = document.createElement("h2");
+    heading.classList.add("dialog-heading");
+    heading.textContent = title;
+
+    form.appendChild(heading);
+
+    fields.forEach(field => {
+        const input = document.createElement(field.type === "textarea" ? "textarea" : "input");
+        input.name = field.name;
+        input.placeholder = field.placeholder;
+
+        if(field.type !== "textarea") input.type = field.type;
+
+        form.appendChild(input);
+    })
+
+    const actions = document.createElement("div");
+    actions.classList.add("actions-button")
+
+    const cancelBtn = document.createElement("button");
+    cancelBtn.textContent = "Cancel"
+    cancelBtn.type = "button";
+    cancelBtn.addEventListener("click", () => dialog.close());
+
+    const addBtn = document.createElement("button");
+    addBtn.textContent = "Add"
+    addBtn.type = "submit"
+
+    actions.append(cancelBtn, addBtn);
+    form.appendChild(actions);
+
+    addBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(form);
+        const data =  Object.fromEntries(formData.entries());
+        onSubmit(data);
+
+        dialog.close();
+        dialog.remove();
+    });
+
+    dialog.appendChild(form);
+    document.body.appendChild(dialog);
+
+    return dialog;
+}
+
+function openProjectModal(createProjects) {
+    const modal = createModal({
+        title: "Add Project",
+        fields: [
+            {
+                name: "title",
+                type: "text",
+                placeholder: "Project Name"
+            }
+        ],
+        onSubmit: (data) => {
+            createProjects(data.title);
+            renderProjects();
+        }
+    });
+
+    modal.showModal();
+}
+
+function openTaskModal(project) {
+    const modal = createModal({
+        title: "Add Task",
+        fields: [
+            { name: "title", type: "text", placeholder: "Title" },
+            { name: "description", type: "textarea", placeholder: "Description" },
+            { name: "dueDate", type: "date"},
+            { name: "priority", type: "text", placeholder: "Priority (low/medium/high)"}
+        ],
+        onSubmit: (data) => {
+            console.log("Task: ", data);
+            project.getTodos();
+        }
+    });
+
+    modal.showModal();
 }
 
--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ createProjects("Home");
 createProjects("Personal");
 createProjects("Work");
 
-renderProjects();
 
 const projects = getProjects();
 const todo1 = todo("Buy Groceries", "Food items, vegetables and snacks", "2026-03-15", "High");
@@ -19,4 +18,5 @@ projects[1].addTodo(todo1);
 projects[1].addTodo(todo1);
 projects[2].addTodo(todo1);
 projects[2].addTodo(todo1);
+renderProjects();
 renderTodos(projects[0]);
